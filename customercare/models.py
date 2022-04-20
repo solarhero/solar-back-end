e2d5efcfc787aab21470e622005973d9f3292e09
Pumphuek <pumphuek@outlook.com>
2022-04-20 11:11:57 +0700

    change models

--- customercare/models.py
+++ customercare/models.py
@@ -5,6 +5,7 @@ from queue import Empty
 from django.db import models
 
 COMMENT_TYPE = [
+    ('interested', 'interested'),
     ('recommend', 'recommend'),
     ('inquiry', 'Sophomore'),
     ('report', 'report'),
